5ae0b64cc987f5164d47126041853cd2a01d0e08
张赫男 <zhanghenan03@meituan.com>
2020-02-15 11:33:23 +0800

    修改几个问题

--- src/main/java/ltd/newbee/mall/service/impl/AdminUserServiceImpl.java
+++ src/main/java/ltd/newbee/mall/service/impl/AdminUserServiceImpl.java
@@ -16,8 +16,8 @@ public class AdminUserServiceImpl implements AdminUserService {
 
     @Override
     public AdminUser login(String userName, String password) {
-//        String passwordMd5 = MD5Util.MD5Encode(password, "UTF-8");
-        return adminUserMapper.login(userName, password);
+        String passwordMd5 = MD5Util.MD5Encode(password, "UTF-8");
+        return adminUserMapper.login(userName, passwordMd5);
     }
 
     @Override

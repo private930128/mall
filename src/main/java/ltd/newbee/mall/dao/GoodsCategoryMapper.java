cfdb9e2c2582b793acefa606d298f69f2b3e577e
张赫男 <zhanghenan03@meituan.com>
2020-02-18 16:59:30 +0800

    品类配置接口

--- src/main/java/ltd/newbee/mall/dao/GoodsCategoryMapper.java
+++ src/main/java/ltd/newbee/mall/dao/GoodsCategoryMapper.java
@@ -28,4 +28,6 @@ public interface GoodsCategoryMapper {
     int deleteBatch(Integer[] ids);
 
     List<GoodsCategory> selectByLevelAndParentIdsAndNumber(@Param("parentIds") List<Long> parentIds, @Param("categoryLevel") int categoryLevel, @Param("number") int number);
+
+    List<GoodsCategory> listCategoryInfo();
 }

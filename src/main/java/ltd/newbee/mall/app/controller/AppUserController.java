25ed7b02cd11d6dbe70e35a0d739ca6b5e5d336f
张赫男 <zhanghenan03@meituan.com>
2020-04-12 17:25:31 +0800

    联调

--- src/main/java/ltd/newbee/mall/app/controller/AppUserController.java
+++ src/main/java/ltd/newbee/mall/app/controller/AppUserController.java
@@ -4,9 +4,11 @@ import com.alibaba.fastjson.JSON;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import ltd.newbee.mall.app.constant.ResultMsgEnum;
+import ltd.newbee.mall.app.dto.UserInfoDto;
 import ltd.newbee.mall.app.dto.UserRegistryDto;
 import ltd.newbee.mall.config.redis.RedisUtil;
 import ltd.newbee.mall.controller.vo.WechatAuthTokenVO;
+import ltd.newbee.mall.dao.MallUserMapper;
 import ltd.newbee.mall.entity.MallUser;
 import ltd.newbee.mall.service.WechatService;
 import ltd.newbee.mall.util.Result;
@@ -15,10 +17,12 @@ import ltd.newbee.mall.util.wxpay.PaymentControllerbak;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
+import java.util.List;
 
 /**
  * 用于v1.0 app用户信息相关交互接口
@@ -35,6 +39,8 @@ public class AppUserController {
     private WechatService wechatService;
     @Autowired
     private RedisUtil redisUtil;
+    @Autowired
+    private MallUserMapper mallUserMapper;
 
     @ApiOperation(value = "用户登录接口")
     @RequestMapping(value = "login", method = RequestMethod.GET)
@@ -44,7 +50,7 @@ public class AppUserController {
         try {
             WechatAuthTokenVO wechatAuthTokenVO = wechatService.wechatLogin(user);
             if (wechatAuthTokenVO == null) {
-                return ResultGenerator.genErrorResult(ResultMsgEnum.MSG_VERIFY_PARAM_IS_NULL.getCode(), ResultMsgEnum.MSG_VERIFY_PARAM_IS_NULL.getMsg());
+                return ResultGenerator.genErrorResult(ResultMsgEnum.USER_NOT_EXIST.getCode(), ResultMsgEnum.USER_NOT_EXIST.getMsg());
             }
             logger.info("login response : wechatAuthTokenVO = {}", JSON.toJSON(wechatAuthTokenVO));
             return ResultGenerator.genSuccessDateResult(wechatAuthTokenVO);
@@ -91,6 +97,35 @@ public class AppUserController {
         } catch (Exception e) {
             e.printStackTrace();
             return ResultGenerator.genFailResult("注册失败");
+        }
+    }
+
+    @ApiOperation(value = "用户信息接口")
+    @RequestMapping(value = "getUserInfo", method = RequestMethod.GET)
+    @ResponseBody
+    public Result getUserInfo(String token) {
+        logger.info("login param : token = {}", token);
+        try {
+            Object object = redisUtil.get(token);
+            logger.info("createOrder getOpenId : object = {}", object);
+            if (object == null) {
+                return ResultGenerator.genErrorResult(ResultMsgEnum.LOGIN_INFO_IS_NULL.getCode(), ResultMsgEnum.LOGIN_INFO_IS_NULL.getMsg());
+            }
+            String openId = object.toString();
+            if (StringUtils.isEmpty(openId)) {
+                return ResultGenerator.genErrorResult(ResultMsgEnum.LOGIN_INFO_IS_NULL.getCode(), ResultMsgEnum.LOGIN_INFO_IS_NULL.getMsg());
+            }
+            List<MallUser> mallUserList = mallUserMapper.selectByOpenId(openId);
+            if (CollectionUtils.isEmpty(mallUserList)) {
+                return ResultGenerator.genErrorResult(ResultMsgEnum.LOGIN_INFO_IS_NULL.getCode(), ResultMsgEnum.LOGIN_INFO_IS_NULL.getMsg());
+            }
+            UserInfoDto userInfoDto = new UserInfoDto();
+            userInfoDto.setLoginName(mallUserList.get(0).getLoginName());
+            userInfoDto.setAddress(mallUserList.get(0).getAddress());
+            return ResultGenerator.genSuccessDateResult(userInfoDto);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResultGenerator.genFailResult("获取用户信息失败");
         }
     }
 }

a029f861e741e834c9e985e08cb84d66e09cf64e
egouwu <wangyufei@nutfin.cn>
2020-08-26 10:11:42 +0800

    修改信息

--- src/main/java/ltd/newbee/mall/manager/PromotionManager.java
+++ src/main/java/ltd/newbee/mall/manager/PromotionManager.java
@@ -8,7 +8,7 @@ import ltd.newbee.mall.entity.NewBeeMallShoppingCartItem;
 public interface PromotionManager {
 
     /**
-     * 计算购物车中的促销活动信息
+     * 计算购物车中的促销活动
      * @param cartItemList 购物车
      */
     List<CartPromotionItem> calcCartPromotion(List<NewBeeMallShoppingCartItem> cartItemList);
